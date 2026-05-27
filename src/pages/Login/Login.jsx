563d8f4c11da3c8f203a9010b940bf27f48c02f4
nahyun8131 <hyuni8131@naver.com>
2026-05-27 14:58:54 +0900

    style: 로그인 페이지에 세종대 로고 적용

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,16 @@
 import LoginForm from './components/LoginForm'
+import sejongLogo from '../../assets/sejong_logo.png'
 import './Login.css'
 
 export default function Login() {
   return (
     <div className="login-page">
       <div className="login-card">
-        <div className="login-card__logo">
-          <span>世</span>
-        </div>
+        <img
+          src={sejongLogo}
+          alt="세종대학교 로고"
+          className="login-card__logo"
+        />
 
         <h1 className="login-card__title">세종마켓</h1>
         <p className="login-card__subtitle">
