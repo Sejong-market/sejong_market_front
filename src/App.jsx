4bc353fc949e0f9628584da76502379d235222ba
nahyun8131 <hyuni8131@naver.com>
2026-06-02 19:24:12 +0900

    refactor: 실시간 채팅 페이지 제거

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AppLayout from './shared/layout/AppLayout'
 import Login from './pages/Login/Login'
 import ProductList from './pages/ProductList/ProductList'
 import MyPage from './pages/MyPage/MyPage'
-import Chat from './pages/Chat/Chat'
 import ProductRegister from './pages/ProductRegister/ProductRegister' 
 
 export default function App() {
@@ -16,7 +15,6 @@ export default function App() {
         <Route path="/products" element={<ProductList />} />
         <Route path="/products/new" element={<ProductRegister />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/chat" element={<Chat />} />
       </Route>
 
       <Route path="*" element={<Navigate to="/products" replace />} />
